e9e70a3510107a5b59cd8a44f919a6cf5ad3e779
LowyShin <lowy@naver.com>
2016-07-01 19:55:08 +0900

    Create kvsPutDiskUsageLinux.sh

---
+++ giipscripts/sh/kvsPutDiskUsageLinux.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Written by Lowy Shin at 20160627
+# System Variables ===============================================
+sk="{{sk}}"
+lssn="{{lssn}}"
+
+# User Variables ===============================================
+factor="DISKUSAGE"
+value=`df -PaT | grep '/' | awk '{print $6}'`
+
+dffs=`df -PaT | grep '/' | awk '{print $1}'`
+dfty=`df -PaT | grep '/' | awk '{print $2}'`
+dfbl=`df -PaT | grep '/' | awk '{print $3}'`
+dfus=`df -PaT | grep '/' | awk '{print $4}'`
+dfav=`df -PaT | grep '/' | awk '{print $5}'`
+dfca=`df -PaT | grep '/' | awk '{print $6}'`
+dfmo=`df -PaT | grep '/' | awk '{print $7}'`
+
+arydffs=(${dffs//'\n'/ })
+arydfty=(${dfty//'\n'/ })
+arydfbl=(${dfbl//'\n'/ })
+arydfus=(${dfus//'\n'/ })
+arydfav=(${dfav//'\n'/ })
+arydfca=(${dfca//'\n'/ })
+arydfmo=(${dfmo//'\n'/ })
+
+array=(${value//'\n'/ })
+valueJSON="{\"df -PaT\":["
+for i in "${!array[@]}"
+do
+	if [ $i -gt 0 ]; then
+		valueJSON="$valueJSON, "
+	fi
+	valueJSON="$valueJSON {\"Filesystem\":\"${arydffs[i]}\", \"Type\":\"${arydfty[i]}\", \"1024-blocks\":${arydfbl[i]}, \"Used\":${arydfus[i]}, \"Available\":${arydfav[i]}, \"Capacity\":\"${arydfca[i]}\", \"Mounted on\":\"${arydfmo[i]}\"}"
+done
+valueJSON="$valueJSON ] }"
+#echo -e $valueJSON
+# Send to KVSAPI Server =========================================
+qs="sk=$sk&type=lssn&key=$lssn&factor=$factor&value=$valueJSON"
+wget "http://giip.littleworld.net/API/kvs/put?$qs"
