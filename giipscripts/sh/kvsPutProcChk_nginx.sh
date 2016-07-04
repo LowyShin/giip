366742612ca48ae076aec50bec2505a27f83deaf
LowyShin <lowy@naver.com>
2016-07-04 17:44:43 +0900

    Update kvsPutProcChk_nginx.sh
    160704 Lowy, fix Wrong type of JSON

--- giipscripts/sh/kvsPutProcChk_nginx.sh
+++ giipscripts/sh/kvsPutProcChk_nginx.sh
@@ -6,8 +6,8 @@ lssn="{{lssn}}"
 
 # User Variables ===============================================
 factor="PROCCHK-NGINX"
-valueJSON=`ps -ef | grep nginx | awk '{print "{\"UID\":\""$1"\",\"PID\":"$2",\"TIME\":\""$7"\",\"PROC\":\""$8"\",\"CMD\":\""$9" "$10" "$11" "$12" "$13"\"}"}'`
-valueJSON="{\"PROCCHK\":[ $valueJSON ]}" | sed -e "s/} {/}, {/g"
+valueJSON=`ps -ef | grep nginx | awk '{print ",{\"UID\":\""$1"\",\"PID\":"$2",\"TIME\":\""$7"\",\"PROC\":\""$8"\",\"CMD\":\""$9" "$10" "$11" "$12" "$13"\"}"}'`
+valueJSON=`echo "{\"PROCCHK\":[ $valueJSON ]}" | sed -e "s/\[ ,/\[ /g"`
 #echo -e $valueJSON
 
 # Send to KVSAPI Server =========================================
