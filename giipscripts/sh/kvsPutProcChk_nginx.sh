b681c5937d36eb915d8ac53b299899e7f1b935de
LowyShin <lowy@naver.com>
2016-07-05 12:32:49 +0900

    160704 Lowy Updated
    Add variables of process name for check
    Add variables and logic of process count
    Remove grep process from process list 
    Add logic put to giip KVS when process count over zero

--- giipscripts/sh/kvsPutProcChk_nginx.sh
+++ giipscripts/sh/kvsPutProcChk_nginx.sh
@@ -6,13 +6,17 @@ lssn="{{lssn}}"
 
 # User Variables ===============================================
 factor="PROCCHK-NGINX"
-valueJSON=`ps -ef | grep nginx | awk '{print ",{\"UID\":\""$1"\",\"PID\":"$2",\"TIME\":\""$7"\",\"PROC\":\""$8"\",\"CMD\":\""$9" "$10" "$11" "$12" "$13"\"}"}'`
-valueJSON=`echo "{\"PROCCHK\":[ $valueJSON ]}" | sed -e "s/\[ ,/\[ /g"`
+ChkProc="nginx"
+CntProc=`ps -ef | grep $ChkProc | grep -v grep | wc -l`
+valueJSON=`ps -ef | grep $ChkProc | grep -v grep | awk '{print ",{\"UID\":\""$1"\",\"PID\":"$2",\"TIME\":\""$7"\",\"PROC\":\""$8"\",\"CMD\":\""$9" "$10" "$11" "$12" "$13"\"}"}'`
+valueJSON=`echo "{\"PROCCNT\":$CntProc,\"PROCCHK\":[ $valueJSON ]}" | sed -e "s/\[ ,/\[ /g"`
 #echo -e $valueJSON
 
 # Send to KVSAPI Server =========================================
 qs="sk=$sk&type=lssn&key=$lssn&factor=$factor&value=$valueJSON"
-wget "http://giip.littleworld.net/API/kvs/put?$qs" -O "giipapi.log"
+if [ $CntProc > 0 ]; then
+    wget "http://giip.littleworld.net/API/kvs/put?$qs" -O "giipapi.log"
+fi
 
 rm -f giipapi.log
 rm -f put?*
