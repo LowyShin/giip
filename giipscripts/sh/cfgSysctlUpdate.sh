72118fc7300097d2814bfe6d120d1e7880f4a696
LowyShin <lowy@naver.com>
2016-07-07 00:03:57 +0900

    160706 Lowy, Update giip KVS field
    You can logging this script action through giip automation.

--- giipscripts/sh/cfgSysctlUpdate.sh
+++ giipscripts/sh/cfgSysctlUpdate.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+# Written by Lowy Shin at 20160627
+# 160706 Lowy, Update ChainLock Count
+# System Variables ===============================================
+sk="{{sk}}"
+lssn="{{lssn}}"
+
+# Custom Variables [start]===============================================
+# for kvs
+factor="SYSCTL"
+chgval="10"
+# Custom Variables [end]===============================================
+{{CustomVariables}}
+
 today=`date +%Y%m%d`
 ofile="/etc/sysctl.conf"
 ofilebak="$ofile.$today"
 findval="net.ipv4.tcp_fin_timeout"
-chgval="10"
 
 if grep -o "$findval" $ofile > /dev/null
 then
@@ -17,4 +29,11 @@ then
 	fi
 
 	sysctl -p
+
+	valueJSON="{\"FACTOR\":\"$factor\",\"DATA\":[{\"FIELD\":\"$findval\",\"ORG\":$oldvalue,\"NEW\":$chgval,\"BAKFILE\":\"$ofilebak\"}]}"
+
+	# Send to KVSAPI Server =========================================
+	qs="sk=$sk&type=lssn&key=$lssn&factor=$factor&value=$valueJSON"
+	wget "http://giip.littleworld.net/API/kvs/put?$qs"
+
 fi
