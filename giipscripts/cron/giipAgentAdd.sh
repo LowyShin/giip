dafb2e981439a5af577fad4bc5d691cb967bafd6
LowyShin <lowy@naver.com>
2016-07-21 17:02:56 +0900

    Update giipAgentAdd.sh

--- giipscripts/cron/giipAgentAdd.sh
+++ giipscripts/cron/giipAgentAdd.sh
@@ -1,4 +1,4 @@
 crontab -l
 (crontab -l ; echo "# 160701 Lowy, for giip")| crontab -
-(crontab -l ; echo "* * * * * bash --login -c 'sh /usr/local/scripts/giipAgent.sh'")| crontab -
+(crontab -l ; echo "* * * * * bash --login -c 'sh /usr/local/giip/scripts/giipAgent.sh'")| crontab -
 crontab -l
