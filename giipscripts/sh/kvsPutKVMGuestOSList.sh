da7b81b978ee6c99ae035340f4b96a8a69928ded
LowyShin <lowy@naver.com>
2016-08-07 14:31:25 +0900

    Create kvsPutKVMGuestOSList.sh

---
+++ giipscripts/sh/kvsPutKVMGuestOSList.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Written by Lowy Shin at 20160807
+# Put Guest OS List on KVM to KVS
+# System Variables ===============================================
+sk="{{sk}}"
+lssn="{{lssn}}"
+
+# User Variables ===============================================
+factor="KVM"
+Attrib="List"
+DataCnt=`virsh list --all | grep [0-9] | wc -l`
+valueJSON=`virsh list --all | grep [0-9] | awk '{print ",{\"ID\":\""$1"\", \"Name\":\""$2"\", \"State\":\""$3"\"}"}'`
+valueJSON=`echo "{\"$factor-$Attrib\":$DataCnt,\"DATA\":[ $valueJSON ]}" | sed -e "s/\[ ,/\[ /g"`
+#echo -e $valueJSON
+
+# Send to KVSAPI Server =========================================
+qs="sk=$sk&type=lssn&key=$lssn&factor=$factor&value=$valueJSON"
+lwAPIURL="https://secure.littleworld.net/API/kvs/kvsput.asp"
+if [ $DataCnt > 0 ]; then
+	curl -w '\n' "$lwAPIURL" --data "$qs" -XPOST
+fi
+
+rm -f giipapi.log
+rm -f put?*
+rm -f 0
