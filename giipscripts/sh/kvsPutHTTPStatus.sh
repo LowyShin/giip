2929f750175ebcc3e7d6b9040f531b99bbc5ce5d
LowyShin <lowy@naver.com>
2016-08-20 21:49:32 +0900

    Create kvsPutHTTPStatus.sh

---
+++ giipscripts/sh/kvsPutHTTPStatus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Written by Lowy Shin at 20160807
+# Put Guest OS List on KVM to KVS
+# System Variables ===============================================
+sk="{{sk}}"
+lssn="{{lssn}}"
+
+# CustomVariables =========================================
+# Copy below and paste to0 customvariables box and edit check URL
+lwURL="giip.littleworld.net"
+Attrib="giip"
+
+# User Variables ===============================================
+factor="HTTPStatus"
+{{CustomVariables}}
+DataCnt=1
+valueJSON=`curl -LI $lwURL -o /dev/null -w '%{http_code}\n' -s`
+valueJSON=`echo "{\"$factor-$Attrib\":$lwURL,\"STATUS\":$valueJSON}" | sed -e "s/\[ ,/\[ /g"`
+#echo -e $valueJSON
+
+# Send to KVSAPI Server =========================================
+qs="sk=$sk&type=lssn&key=$lssn&factor=$factor-$Attrib&value=$valueJSON"
+lwAPIURL="https://secure.littleworld.net/API/kvs/kvsput.asp"
+if [ $DataCnt > 0 ]; then
+	curl -w '\n' "$lwAPIURL" --data "$qs" -XPOST
+fi
+
+rm -f giipapi.log
+rm -f put?*
+rm -f 0
+
