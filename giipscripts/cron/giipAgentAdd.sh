548e65e056a578c6c7522d1eac7395e441afa49b
Lowy Shin <lowyshin@littleworld.net>
2017-06-22 15:11:25 +0900

    Update giipAgentAdd.sh

--- giipscripts/cron/giipAgentAdd.sh
+++ giipscripts/cron/giipAgentAdd.sh
@@ -1,7 +1,15 @@
+# You can see secret key in service page of giip
+sk="<SecretKey>"
+# if you registered logical server name same as hostname then below, or put your label name
+lb=`hostname`
+
+# Append to crontab
 crontab -l
 (crontab -l ; echo "# 160701 Lowy, for giip")| crontab -
 (crontab -l ; echo "* * * * * bash --login -c 'sh /usr/local/giip/scripts/giipAgent.sh'")| crontab -
 crontab -l
 
+# make file
 mkdir -p /usr/local/giip/scripts
-vi /usr/local/giip/scripts/giipAgent.sh
+# download giip agent
+wget "http://giip.littleworld.net/api/agent/getbylabel?op=giipAgentlinux&sk=$sk&lb=$lb" -O /usr/local/giip/scripts/giipAgent.sh
