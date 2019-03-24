66f68597896520db77346d37f4a68387b7260299
Lowy Shin <lowyshin@littleworld.net>
2019-03-24 15:31:01 +0900

    Update giipAgentAdd.sh

--- giipscripts/cron/giipAgentAdd.sh
+++ giipscripts/cron/giipAgentAdd.sh
@@ -13,3 +13,23 @@ crontab -l
 mkdir -p /usr/local/giip/scripts
 # download giip agent
 wget "http://giipapi.littleworld.net/api/agent/getbylabel?op=giipAgentlinux&sk=$sk&lb=$lb" -O /usr/local/giip/scripts/giipAgent.sh
+
+# check and install dos2unix
+ostype=`head -n 1 /etc/issue | awk '{print $1}'`
+if [ $ostype = "Ubuntu" ];then
+	os=`lsb_release -d`
+	os=`echo "$os"| sed -e "s/Description\://g"`
+
+	if [ ${RESULT} != 0 ];then
+		apt-get install -y dos2unix
+	fi
+else
+	os=`cat /etc/redhat-release`
+
+	if [ ${RESULT} != 0 ];then
+		yum install -y dos2unix
+	fi
+fi
+
+# convert giipScript
+dos2unix /usr/local/giip/scripts/giipAgent.sh
